c7e2c54f3acc7c89b42c8733cd32bdd8c3ec4ebd
iamnachiketh <nachikethkc@gmail.com>
2025-03-03 14:13:16 +0530

    [Nachiketha]Added: Implemented DDL statments like create table, alter table, truncate, drop

---
+++ DDL.sql
@@ -0,0 +1,35 @@
+-- DDL(data definition language)
+-- Used to define an object in a database.
+
+-- Create a table
+
+CREATE TABLE employee (
+    emp_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    department VARCHAR(50),
+    salary NUMERIC(10, 2),
+    hire_date DATE
+);
+
+CREATE TABLE department (
+    dept_id SERIAL PRIMARY KEY,
+    dept_name VARCHAR(50)
+);
+
+-- Alter a table
+
+ALTER TABLE employee ADD COLUMN email VARCHAR(100) NOT NULL UNIQUE;
+
+ALTER TABLE employee 
+ALTER COLUMN first_name TYPE VARCHAR(100);
+
+
+-- Truncate a table
+
+TRUNCATE TABLE employee;
+
+
+-- Drop a table
+
+DROP TABLE employee;
